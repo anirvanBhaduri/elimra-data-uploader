17fbe7ab5d58f51b4d39173318eeb2fdaebf108c
Anirvan Bhaduri <anirvan.bhaduri@gmail.com>
2021-03-22 18:53:56 +0100

    Updated setup script.
    Fixed issue with logs folder not being present.

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,4 @@
 @echo off
 echo "Setting up the application"
-cmd /k "virtualenv .virtualenv & .\.virtualenv\Scripts\activate & COPY .env.sample .env & .\.virtualenv\Scripts\pip.exe install -r requirements.txt"
-exit
+cmd /k "py -m virtualenv .virtualenv & .\.virtualenv\Scripts\activate & COPY .env.sample .env & .\.virtualenv\Scripts\pip.exe install -r requirements.txt"
+ex-m virtualenv
