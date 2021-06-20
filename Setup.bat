1d096601ecc9306b986f583bacc135676fa2b624
Anirvan <anirvan.bhaduri@gmail.com>
2021-06-20 18:03:15 +1200

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,4 @@
 @echo off
 echo "Setting up the application"
-cmd /k "cd frontend & docker run -w=/app --volume=$(pwd)/:/app node:alpine npm install & docker run -w=/app --volume=$(pwd)/:/app node:alpine npm run build"
+cmd /k "cd frontend & docker run -w=/app --volume=%CD%/:/app node:alpine npm install & docker run -w=/app --volume=%CD%/:/app node:alpine npm run build"
 ex-m virtualenv
