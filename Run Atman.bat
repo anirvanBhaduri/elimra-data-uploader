eebd8b84bd2f364c1b79b559bd88b7cabc5aace4
Anirvan <Anirvan@DESKTOP-N6QPTGR>
2021-10-20 20:14:31 +1300

    Modified Bat files to make it easy to run.

---
+++ Run Atman.bat
@@ -0,0 +1,6 @@
+@echo off
+echo "Running the application. Please do not close this command prompt!"
+echo "Go to .\logger\logs to see the logs."
+cd backend
+npm run atman
+pause
--- Run Bosch.bat
+++ Run Bosch.bat
@@ -1,5 +1,6 @@
 @echo off
 echo "Running the application. Please do not close this command prompt!"
 echo "Go to .\logger\logs to see the logs."
-cmd /k ".\.virtualenv\Scripts\activate & py main.py"
+cd backend
+npm run bosch
 pause
