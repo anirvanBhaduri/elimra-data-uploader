c3513272b8ca5efb5af2a39c8406d184238499e2
Anirvan <Anirvan@DESKTOP-N6QPTGR>
2021-11-23 04:06:33 +1300

    Updated setup script to now do everything up till running the app.

--- Setup.bat
+++ Setup.bat
@@ -1,8 +1,5 @@
 @echo off
 echo "Setting up the application"
+mkdir backend\build
 cd frontend
-npm i
-npm run build
-cd ../backend 
-npm i
-npm run start
+npm i && npm run pure:build && xcopy /s/e/y %CD%\build %CD%\..\backend\build && cd ..\backend && npm i && npm run start
